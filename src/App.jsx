d76203c74aa5fa573122c1e2f839e48a23dd3d18
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-11 14:02:46 -0800

    add temperature slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
+import { useState } from "react";
+
 const App = ()=>{
+  const [temperatureValue, setTemperatureValue] = useState(0);
+
   return (
-    <div>hello react</div>
+    <div className="app-container">
+      
+      <div className="temperature-display-container">
+        <div className="temperature-display">{temperatureValue}°C</div>
+      </div>
+      <div className="button-container">
+        <button onClick={()=> setTemperatureValue((prevValue)=> prevValue + 1)}>+</button>
+        <button onClick={()=> setTemperatureValue((prevValue)=> prevValue - 1)}>-</button>
+      </div>
+    </div>
   )
 }
 export default App;
