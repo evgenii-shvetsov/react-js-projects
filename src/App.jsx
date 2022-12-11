80124df6b4a6bb06b224c07288fbedd6e30e4280
evgenii-shvetsov <sea.dev91@gmail.com>
2022-12-11 14:31:53 -0800

    add different temperature levels

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,36 @@ import React from "react";
 import { useState } from "react";
 
 const App = ()=>{
-  const [temperatureValue, setTemperatureValue] = useState(0);
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold")
+
+  const increaseTemperature = ()=>{
+    const newTemperature = temperatureValue + 1 ;
+    if(newTemperature >= 15) {
+      setTemperatureColor('hot')
+    
+    }
+    setTemperatureValue(newTemperature)
+  }
+
+  const decreaseTemperature = ()=>{
+    const newTemperature = temperatureValue - 1 ;
+    if(newTemperature < 15) {
+      setTemperatureColor('cold')
+    
+    }
+    setTemperatureValue(newTemperature)
+  }
 
   return (
     <div className="app-container">
       
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperatureValue}°C</div>
+        <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>
       </div>
       <div className="button-container">
-        <button onClick={()=> setTemperatureValue((prevValue)=> prevValue + 1)}>+</button>
-        <button onClick={()=> setTemperatureValue((prevValue)=> prevValue - 1)}>-</button>
+        <button onClick={()=>increaseTemperature()}>+</button>
+        <button onClick={()=> decreaseTemperature()}>-</button>
       </div>
     </div>
   )
